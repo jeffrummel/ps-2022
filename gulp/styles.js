a16b5460440773540e6ec62822edbe409a5ebe94
Jeff <jeff@jeffrummel.com>
2022-01-06 11:04:25 -0500

    Initial commit

---
+++ gulp/styles.js
@@ -0,0 +1,29 @@
+var project = require('./_project.js')
+var gulp    = require('gulp')
+var sass    = require('gulp-sass')(require('node-sass'));
+var postcss = require('gulp-postcss');
+
+//  Plugins
+var precss  = require('precss');
+var csso    = require('postcss-csso');
+var pixelstorem = require('postcss-pixels-to-rem');
+
+gulp.task('styles', function() {
+  return gulp.src(project.src + '/css/*.scss')
+    .pipe(sass({
+      outputStyle: 'uncompressed'
+    }).on('error', sass.logError))
+    .pipe(gulp.dest(project.dest + '/css/'))
+})
+gulp.task('styles:production', function() {
+  var postProcessor = [
+    pixelstorem({
+      base: 16
+    }),
+    precss(),
+    csso({forceMediaMerge: true})
+  ];
+  return gulp.src(project.dest + '/css/*.css')
+    .pipe(postcss(postProcessor))
+    .pipe(gulp.dest(project.dest + '/css'))
+})
