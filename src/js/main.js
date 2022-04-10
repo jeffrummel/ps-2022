9b3c7f413f932b26b2ef6dd5ddd9801328cfdcff
Jeff <jeff@jeffrummel.com>
2022-04-10 11:15:53 -0400

    updates cards

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,12 @@
 console.log('Let\'s write some javascript!')
+
+const btn = document.querySelectorAll('.marq--cta');
+
+btn.forEach(anchor => {
+	anchor.addEventListener('click', function (e) {
+		e.preventDefault()
+		document.querySelector(this.getAttribute('href')).scrollIntoView({
+			behavior: 'smooth'
+		})
+	})
+})
