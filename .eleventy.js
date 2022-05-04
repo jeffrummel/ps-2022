b828dfce58fe14ab3c86aeaf6c966ededd449cbd
Jeff <jeff@jeffrummel.com>
2022-05-04 14:10:09 -0400

    adds components

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,48 @@
+const Image        = require("@11ty/eleventy-img");
 const { DateTime } = require("luxon");
+
+async function imageShortcode( classes, src, alt, sizes = '100vw', elClass) {
+  // if(alt === undifined) {
+  //   throw new Error(`Missing \`alt\` on responsiveimage from: ${src}`)
+  // }
+  let metadata = await Image(src.includes('http') ? src : `./src/_cms${src}`, {
+
+  // let metadata = await Image(src, {
+    widths: [600, 1280, 2880],
+    formats: ['webp', 'jpeg'],
+    // outputDir: './dist/img/uploads',
+    // urlPath: '/img/uploads/',
+    // formats: ['webp', 'gif']
+    sharpOptions: {
+      animated: true
+    },  
+    filenameFormat: function (id, src, width, format, options) {
+      const extension = path.extname(src);
+      const name = path.basename(src, extension);
+  
+      return `${name}-${width}w.${format}`;
+    }    
+
+  })
+  let lowsrc  = metadata.jpeg[0]
+  let highsrc = metadata.jpeg[metadata.jpeg.length - 1]
+  // let elClass = "derp"
+
+
+  return `<picture class="${classes}">
+    ${Object.values(metadata).map(imageFormat => {
+      return `  <source class="lazy" type="${imageFormat[0].sourceType}" srcset="${imageFormat.map(entry => entry.srcset).join(", ")}" sizes="${sizes}">`;
+    }).join("\n")}
+      <img
+        src="${lowsrc.url}"
+        alt="${alt}"
+        class="${elClass}"
+        loading="lazy"
+        decoding="async">
+    </picture>`;
+
+}
+
 
 module.exports = function(config) {  
   
@@ -9,10 +53,22 @@ module.exports = function(config) {
     breaks: true,
     linkify: true
   };
+  // image shortcodes
+  config.addNunjucksAsyncShortcode("image", imageShortcode);
+  config.addLiquidShortcode("image", imageShortcode);
+  config.addJavaScriptFunction("image", imageShortcode);
+  
   config.addFilter('copyDate', dateObj => {
     return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy');
   });
   config.setLibrary("md", markdownIt(options));
+
+  const mdRender = new markdownIt(options);
+  config.addFilter("mdRender", function(rawString) {
+    return mdRender.render(rawString);
+  });
+
+
   config.setDynamicPermalinks(false);
   config.addPassthroughCopy("favicon.ico");
   config.addPassthroughCopy('img');
